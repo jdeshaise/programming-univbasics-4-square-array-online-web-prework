516f54ea1065a8f4870f7efcc77281c8d0cacda0
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-20 01:36:18 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
+/while as the iterator !!!
+#take an array of numbers
+#square each numbers
+#return a ****new ******* arraywith square numbers
+
+#whike(Conclusional) iterator
+ #loop
+ #while  <<<Conclusion>>> do
+     ###executtion code
+     # end
+
+
+
 def square_array(array)
-  # your code here
+  new_array =[]
+
+  counter = 0
+
 end
